a17afd64507b42d9b5993d3a425d951cddf6d808
khacnhuan14102002 <20130090@st.hcmuaf.edu.vn>
2024-01-14 17:02:02 +0700

    Store

--- ProjectFinal.NET/Controllers/StoreController.cs
+++ ProjectFinal.NET/Controllers/StoreController.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectFinal.NET.Data;
 
 namespace ProjectFinal.NET.Controllers
 {
     public class StoreController : Controller
     {
-        public IActionResult Store()
+
+        public readonly DotnetContext db;
+        public StoreController(DotnetContext context)
         {
-            return View();
-        } 
+            db = context;
+        }
+
+        public IActionResult Store(int? brandId, int? categoryId)
+        {
+            ViewBag.Categories = GetCategories();
+            ViewBag.Brands = GetBrands();
+
+            IQueryable<Product> products = db.Products.AsQueryable();
+
+            if (brandId.HasValue)
+            {
+                products = products.Where(p => p.IdBrand == brandId.Value);
+            }
+
+            if (categoryId.HasValue)
+            {
+                products = products.Where(p => p.IdCate == categoryId.Value);
+            }
+
+            List<Product> filteredProducts = products.ToList();
+
+            return View(filteredProducts);
+        }
+        private List<Brand> GetBrands()
+        {
+            // Replace this with your actual logic to fetch brands from the database
+            return db.Brands.ToList();
+        }
+        private List<Category> GetCategories()
+        {
+            // Replace this with your actual logic to fetch brands from the database
+            return db.Categories.ToList();
+        }
+
     }
 }
